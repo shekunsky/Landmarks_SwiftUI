8abf6b4517f82ee5da44afafa5b08fa7fe9c8d18
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-10-26 15:56:01 +0300

    Complete the category home view

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -12,12 +12,16 @@ class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
     
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
     var categories: [String: [Landmark]] {
-            Dictionary(
-                grouping: landmarks,
-                by: { $0.category.rawValue }
-            )
-        }
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
