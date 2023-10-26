4751d549e6ff6817297fe8c71555894b9f0d739c
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-10-26 15:26:48 +0300

    Create a category list

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
-    @Published var hikes: [Hike] = load("hikeData.json")
+    var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+            Dictionary(
+                grouping: landmarks,
+                by: { $0.category.rawValue }
+            )
+        }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
