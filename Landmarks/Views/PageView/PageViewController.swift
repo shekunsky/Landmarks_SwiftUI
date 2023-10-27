88e58b43d40e6504e1f88f7cefb8b2fe765dacd8
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-10-27 15:16:46 +0300

    Track the page in a SwiftUI view’s state

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
+    @Binding var currentPage: Int
     
     /// SwiftUI calls this makeCoordinator() method before makeUIViewController(context:), so that you have access to the coordinator object when configuring your view controller.
     func makeCoordinator() -> Coordinator {
@@ -24,13 +25,14 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             navigationOrientation: .horizontal)
         
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+            [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
         
     }
     
@@ -38,7 +40,7 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     /// SwiftUI manages UIViewControllerRepresentable type’s coordinator, and provides it as part of the context when calling the methods created above.
     ///
     /// You can use this coordinator to implement common Cocoa patterns, such as delegates, data sources, and responding to user events via target-action.
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         var controllers = [UIViewController]()
         
@@ -47,7 +49,8 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             controllers = parent.pages.map { UIHostingController(rootView: $0) }
         }
         
-        func pageViewController(_ pageViewController: UIPageViewController,
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
             viewControllerBefore viewController: UIViewController) -> UIViewController? {
             guard let index = controllers.firstIndex(of: viewController) else {
                 return nil
@@ -59,7 +62,8 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
         }
         
         
-        func pageViewController(_ pageViewController: UIPageViewController,
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
             viewControllerAfter viewController: UIViewController) -> UIViewController? {
             guard let index = controllers.firstIndex(of: viewController) else {
                 return nil
@@ -69,5 +73,17 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             }
             return controllers[index + 1]
         }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool) {
+                if completed,
+                   let visibleViewController = pageViewController.viewControllers?.first,
+                   let index = controllers.firstIndex(of: visibleViewController) {
+                    parent.currentPage = index
+                }
+            }
     }
 }
