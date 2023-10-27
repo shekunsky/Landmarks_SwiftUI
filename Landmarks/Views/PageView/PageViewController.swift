f3b0dfa52d80f551aa525ba454c1db040d1c77d7
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-10-27 14:58:13 +0300

    Create the view controller’s data source

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -12,18 +12,62 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
+    /// SwiftUI calls this makeCoordinator() method before makeUIViewController(context:), so that you have access to the coordinator object when configuring your view controller.
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     /// SwiftUI calls this method a single time when it’s ready to display the view, and then manages the view controller’s life cycle.
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
         
+        pageViewController.dataSource = context.coordinator
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+        
+    }
+    
+    
+    /// SwiftUI manages UIViewControllerRepresentable type’s coordinator, and provides it as part of the context when calling the methods created above.
+    ///
+    /// You can use this coordinator to implement common Cocoa patterns, such as delegates, data sources, and responding to user events via target-action.
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+        
+        
+        func pageViewController(_ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
     }
 }
